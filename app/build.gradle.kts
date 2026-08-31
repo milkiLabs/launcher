8f51c8512bf9f90e7860ae6dca21bc8602322e72
MuhammadSwa <muhammadatefsw@protonmail.com>
2026-08-31 14:19:24 +0300

    version bump

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,8 +8,8 @@ plugins {
 import java.util.Properties
 import java.io.FileInputStream
 
-val appVersionCode = 10301
-val appVersionName = "1.3.1"
+val appVersionCode = 10303
+val appVersionName = "1.3.3"
 
 // LOAD KEYSTORE PROPERTIES
 val keystorePropertiesFile = rootProject.file("keystore.properties")
